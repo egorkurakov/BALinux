b25dadff8a85df288e9f426de5be1a8e96d4fd0e
egorkurakov <egor@live.ru>
2016-12-21 19:59:59 +0300

    1

---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get updrade
+sudo apt-get install sysstat -y
+sudo apt-get install nginx -y
+sudo apt-get install apache2 -y
+sudo apt-get install python3 -y
+sudo rm -rf /etc/apache2/sites-available/*
+sudo rm -rf /etc/apache2/sites-enables/*
+sudo rm -rf /etc/nginx/sites-available/*
+sudo rm -rf /etc/nginx/sites-enables/*
+sudo cp src/sysinfo.conf /etc/apache2/sistes-available
+sudo ln -s /etc/apache2/sites-available/sysinfo.conf /etc/apache2/sites-enabled/sysinfo.conf
+sudo cp src/BALinux.conf /etc/apache2/sites-available
+sudo ln -s /etc/nginx/sites-available/BALinux.conf /etc/nginx/sites-enables/BALinux.conf
+sudo cp src/readTCPDUMP /var/www
+sudo cp src/readIOSTAT /var/www
+sudo cp src/SysRec.py /var/www
+sudo chmod +x /var/www/readTCPDUMP
+sudo chmod +x /var/www/readIOSTAT
+sudo chmod +x /var/www/SysRev
+sudo crontab src/MyCrontab
+sudo service apache2 restart
+sudo service nginx restart
